65b51b0a49c234047b23646c6c595a8b62969ba4
johnhaas <haasj22@up.edu>
2019-09-28 14:46:36 -0700

    TheSetupGUI -SurfaceView includued

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,8 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ImageView iv = (ImageView)findViewById(R.id.boardImageView);
-        //iv.setImageResource(R.drawable.stratego_9);
-
     }
 }
