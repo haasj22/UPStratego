1bc9d4d30280aecb291f58ec5aeac6d69ea12c1f
johnhaas <haasj22@up.edu>
2019-10-01 12:16:10 -0700

    added self-drawn surfaceView GUI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView iv = (ImageView)findViewById(R.id.boardImageView);
-        iv.setImageResource(R.drawable.corrected_board);
 
     }
 }
